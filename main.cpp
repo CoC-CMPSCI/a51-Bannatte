544361b9d38ca9f5152831da566d3de156a9f8bf
Bannatte <32626837+Bannatte@users.noreply.github.com>
2026-03-31 23:21:07 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// Complete the following program.
 #include <iostream>
 #include <iomanip>
 
@@ -6,32 +5,28 @@ using namespace std;
 
 int main()
 {
-    // TODO: declare variable n for the exponent input
+    // Declare variable n for the exponent input
     int n;
     cout << "Enter integer n: ";
     cin >> n;
     
-    // TODO: read n from standard input
+    // Initialize output
     double output = 1.0;
+
     if (n > 0) {
+        // Positive exponent: multiply by 2, n times
         for (int i = 0; i < n; ++i) {
-            result *= 2;
+            output *= 2;
         }
     } else if (n < 0) {
+        // Negative exponent: divide by 2, |n| times
         for (int i = 0; i < -n; ++i) {
-            result /= 2;
+            output /= 2;
         }
-    } else {
-        continue;
     }
 
-    // TODO: compute 2 to the power of n using a loop (no cmath pow)
-    // For positive n: multiply result by 2, n times
-    // For negative n: divide result by 2, |n| times
-
     cout << setprecision(5) << fixed;
-    cout << "2 to the power of " << n << " is equal too " << output << endl;
+    cout << "2 to the power of " << n << " is " << output << endl;
 
     return 0;
-
 }
